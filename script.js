8fa6d356161d92f967899f314261c8ef5f21b239
Duvandoe <andresdoe30@gmail.com>
2024-11-08 09:41:42 -0500

    Editado responsive

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav ul li a[href*=' + id + ' ]').classList.add(active)
+                document.querySelector('header nav ul li a[href*=' + id + ' ]').classList.add('active');
             })
         }
     })
@@ -22,5 +22,5 @@ window.onscroll = () => {
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle(active);
+    navbar.classList.toggle('active');
 }
