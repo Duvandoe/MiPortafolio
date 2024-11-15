c4eccff3a2728b943de8c8feaa315016787b30e1
Duvandoe <andresdoe30@gmail.com>
2024-11-15 01:24:12 -0500

    uh

--- js/descargar-hoja-de-vida.js
+++ js/descargar-hoja-de-vida.js
@@ -1,6 +1,6 @@
 function descargarHojaDeVida() {
     const enlace = document.createElement("a"); // Crear un elemento <a>
-    enlace.href = "/public/hoja-de-vida-duvan-cardona.pdf"; // Ruta del archivo PDF
+    enlace.href = "/hoja-de-vida-duvan-cardona.pdf"; // Ruta del archivo PDF
     enlace.download = "Hoja_de_Vida_Duvan_Cardona.pdf"; // Nombre del archivo para la descarga
     enlace.click(); // Disparar la descarga
   }
