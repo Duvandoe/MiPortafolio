51747b7c2abc30886c14862c18b4184fcbcc1c91
Duvandoe <andresdoe30@gmail.com>
2024-11-15 00:57:41 -0500

    Orden de codigo

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav ul li a');
 
